067028c0d68bcee6a6a8846b797898fd437e2a6f
Darkest Surface <74876947+DaRkSurface@users.noreply.github.com>
2021-12-13 19:37:22 +0100

    Create lazydrk.py

---
+++ lazydrk.py
@@ -0,0 +1,6 @@
+#####################################################
+## LazyDrk (ldrk)                                  ##
+## Made to connect all my programs in on place.    ##
+## https://voidsecurity.ml                         ##
+## Coded by: drk                                   ##
+#####################################################
